da92e19f3d20fc83cf987e1abfb9ba676ffc36f6
wilgru <william.gruszka@gmail.com>
2022-07-08 21:04:52 +1000

    create navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+const Navigation = () => {
+    return (
+        <nav className="App-nav">
+            <button>Who Am I</button>
+            <button>Work</button>
+            <button>Touch Base</button>
+        </nav>
+    )
+};
+
+export default Navigation;
