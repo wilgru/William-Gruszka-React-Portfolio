12a46b9f7441d48a86cb094549f6dda1db632cf8
wilgru <william.gruszka@gmail.com>
2022-07-08 21:04:33 +1000

    import Navigation component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import Navigation from './Navigation';
+
+const Header = () => {
+    
+    return (
+        <div className="App-header">
+            <h1>William Gruszka</h1>
+            <Navigation/>
+        </div>
+    )
+};
+
+export default Header;
