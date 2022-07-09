6a60644ab8636ece753779f4381b2e5b291a5af0
wilgru <william.gruszka@gmail.com>
2022-07-09 14:56:24 +1000

    added plaque class

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,9 @@
 const Navigation = () => {
     return (
         <nav className="App-nav">
-            <button>Who Am I</button>
-            <button>Work</button>
-            <button>Touch Base</button>
+            <button className="plaque plaque-btn">Who Am I</button>
+            <button className="plaque plaque-btn">Work</button>
+            <button className="plaque plaque-btn">Touch Base</button>
         </nav>
     )
 };
