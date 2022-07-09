9ede1cd79e5d0e03449e2adb6c3035127fd493f1
wilgru <william.gruszka@gmail.com>
2022-07-09 15:53:46 +1000

    made header appear conditionally

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,9 @@ import project1image from '../project_1.png';
 import project2image from '../project_2.png';
 import project3image from '../project_3.png';
 
-function App(props) {
+function App() {
   const [titleIndex, setTitleIndex] = useState(0);
+  const [display, setDisplay] = useState(false);
 
   useEffect(() => {
     const cardList = document.getElementsByClassName('card');
@@ -26,22 +27,30 @@ function App(props) {
           closestIndex = i;
         }
       }
-      console.log(closestIndex);
+      // console.log(closestIndex);
       setTitleIndex(closestIndex);
+      
+      if (closestIndex > 0) {
+        setDisplay(true);
+      } else {
+        setDisplay(false);
+      };
     });
 
   }, []);
 
   return (
     <div className="App">
-      <Header/>
+      <Header display={display}/>
       <DynamicTitle titleIndex={titleIndex}/>
       <div className="page-stream">
-        <div className="card hero-card">
-          <h1>William Gruszka</h1>
-          <h2>Junior Web Developer</h2>
+        <div className="card hero-card card-bottom-buffer">
+          <section>
+            <h1>William Gruszka</h1>
+            <h2>Junior Web Developer</h2>
+          </section>
         </div>
-        <div className="card who-am-i-card">
+        <div className="card who-am-i-card card-bottom-buffer">
           <p>
             Friendly and aspiring full-stack web developer motivated by a semantic and UX-conscious approach to web development and responsive web design, with interests in Progressive Web Apps and emerging technologies. Comes from a background in fast-paced optical retail, developing skills in communication, teamwork and attention to detail. Currently studying a certificate in web development through USYD’s Coding Bootcamp, developing skills with industry leading technologies like MERN Stack, PWA tooling including Webpack and Workbox, CSS libraries including Bootstrap, MVC and ORM/ODM.<br/>
             <br/>
@@ -49,13 +58,13 @@ function App(props) {
           </p>
           <img className='shadow' src={headshot}></img>
         </div>
-        <div className="card work-card">
+        <div className="card work-card card-bottom-buffer">
           <Project image={project1image} title={"Giphy Guessing Game"} tags={['GiphyAPI', 'WordsAPI']}/>
           <Project image={project2image} title={"Generic Weather App"} tags={['OpenWeatherAPI', 'Bootstrap']}/>
           <Project image={project3image} title={"ProjectHub"} tags={['Node.js', 'Express.js', 'Sequelize', 'Bootstrap']}/>
         </div>
         <div className="card touch-base-card">
-        <form autocomplete="false">
+        <form autoComplete="false">
           <input type="email" name="name" placeholder='Your email here...'/>
           <textarea placeholder='Your message here...'></textarea>
           <input className="plaque plaque-btn" type="submit" value="Submit"/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import Navigation from './Navigation';
 
-const Header = () => {
-    
+const Header = (props) => {
+    console.log(props.display);
+
+    let hStyle = {
+        opacity: 1
+    }
+
+    if (props.display) {
+        hStyle.opacity = 1
+    } else {
+        hStyle.opacity = 0
+    }
+
     return (
         <div className="App-header">
-            <h1 className='plaque' >William Gruszka</h1>
+            <h1 style={hStyle} className='plaque' >William Gruszka</h1>
             <Navigation/>
         </div>
     )
