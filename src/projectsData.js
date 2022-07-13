f5b71654b2b1e37407735b28dc1de1af247b50d3
wilgru <william.gruszka@gmail.com>
2022-07-13 18:19:31 +1000

    added file with project data

---
+++ src/projectsData.js
@@ -0,0 +1,29 @@
+import project1image from './project_1.png';
+import project2image from './project_2.png';
+import project3image from './project_3.png';
+
+const projectData = [
+    {
+        title: "Giphy Guessing Game",
+        image: project1image,
+        tags: ['GiphyAPI', 'WordsAPI'],
+        githubLink: "https://github.com/wilgru/Giphy-Guessing-Game",
+        deployedLink: "wilgru.github.io/group-f-project-1-giphy-guessing-game/"
+    },
+    {
+        title: "Generic Weather App",
+        image: project2image,
+        tags: ['OpenWeatherAPI', 'Bootstrap'],
+        githubLink: "https://github.com/wilgru/USYDBC-WG-06-Weather-Dashboard-WG",
+        deployedLink: "https://wilgru.github.io/Weather-Dashboard-WG/"
+    },
+    {
+        title: "ProjectHub",
+        image: project3image,
+        tags: ['Node.js', 'Express.js', 'Sequelize', 'Bootstrap'],
+        githubLink: "https://github.com/wilgru/projecthub",
+        deployedLink: "projecthub-crm.herokuapp.com/"
+    },
+]
+
+export default projectData;
