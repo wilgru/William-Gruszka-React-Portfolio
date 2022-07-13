aeef5fdb0f08d13148a1edb6f0c8a168e640808d
wilgru <william.gruszka@gmail.com>
2022-07-13 18:21:51 +1000

    added links to project components

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,32 @@
-import project_3 from '../project_3.png';
-
-const tagLoop = (tags) => {
+const Project = (props) => {
+  const tagLoop = (tags) => {
     let els = [];
 
     for (let i = 0; i < tags.length; i++) {
-        els.push(<p className='plaque plaque-tag'>{tags[i]}</p>)
+      els.push(
+        <p className='plaque plaque-tag'>{tags[i]}</p>
+      )
     }
-
     return els
-}
+  }
 
-const Project = (props) => {
-    return (
-        <div className="project-card">
-            <img className="shadow" src={props.image}></img>
-            <section>
-                {tagLoop(props.tags)}
-            </section>
-            <p>{props.title}</p>
+  return (
+    <div className="project-card">
+    <div className="project-preview">
+        <img className="shadow" src={props.image}></img>
+        <div className="project-overlay shadow">
+            <div className="overlay-btns-container">
+            <a className="plaque" href={props.githubLink}>GitHub</a>
+            <a className="plaque" href={props.deployedLink}>Link</a>
+          </div>
         </div>
-    )
+      </div>
+      <section>
+        {tagLoop(props.tags)}
+      </section>
+      <p>{props.title}</p>
+    </div>
+  )
 };
 
 export default Project;
