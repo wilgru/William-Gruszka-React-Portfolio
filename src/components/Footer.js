c26b167beaa0a3ba79772cbdc9c4777fead98f8f
wilgru <william.gruszka@gmail.com>
2022-07-13 18:21:16 +1000

    created footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+const Footer = () => {
+    return (
+        <div className="App-footer">
+            <a>GitHub</a>
+            <a>LinkedIn</a>
+            <a>Resume</a>
+            <p>William Gruszka 2022</p>
+        </div>
+    )
+};
+
+export default Footer;
