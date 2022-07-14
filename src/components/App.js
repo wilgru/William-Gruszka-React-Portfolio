612c60d9e6039b8cb898c1874c36d835b1e9060b
wilgru <william.gruszka@gmail.com>
2022-07-14 10:34:31 +1000

    added ids

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,6 @@ import headshot from '../headshot.jpg';
 
 import Project from './Project';
 import projectData from '../projectsData';
-import project1image from '../project_1.png';
-import project2image from '../project_2.png';
-import project3image from '../project_3.png';
 
 function App() {
   const [titleIndex, setTitleIndex] = useState(0);
@@ -60,13 +57,13 @@ function App() {
         <div className='header-and-card-stream'>
           <DynamicTitle titleIndex={titleIndex}/>
           <div className="card-stream">
-            <div className="card hero-card card-bottom-buffer">
+            <div id='hero-card' className="card hero-card card-bottom-buffer">
               <section>
                 <h1>William Gruszka</h1>
                 <h2>Junior Web Developer</h2>
               </section>
             </div>
-            <div className="card who-am-i-card card-bottom-buffer">
+            <div id='who-am-i-card' className="card who-am-i-card card-bottom-buffer">
               <p>
                 Friendly and aspiring full-stack web developer motivated by a semantic and UX-conscious approach to web development and responsive web design, with interests in Progressive Web Apps and emerging technologies. Comes from a background in fast-paced optical retail, developing skills in communication, teamwork and attention to detail. Currently studying a certificate in web development through USYD’s Coding Bootcamp, developing skills with industry leading technologies like MERN Stack, PWA tooling including Webpack and Workbox, CSS libraries including Bootstrap, MVC and ORM/ODM.<br/>
                 <br/>
@@ -74,13 +71,10 @@ function App() {
               </p>
               <img className='shadow' src={headshot}></img>
             </div>
-            <div className="card work-card card-bottom-buffer">
-              {/* <Project image={project1image} title={"Giphy Guessing Game"} githubLink={} deployedLink={} tags={['GiphyAPI', 'WordsAPI']}/>
-              <Project image={project2image} title={"Generic Weather App"} githubLink={} deployedLink={} tags={['OpenWeatherAPI', 'Bootstrap']}/>
-              <Project image={project3image} title={"ProjectHub"} githubLink={} deployedLink={} tags={['Node.js', 'Express.js', 'Sequelize', 'Bootstrap']}/> */}
+            <div id='work-card' className="card work-card card-bottom-buffer">
               {projectLoop(projectData)}
             </div>
-            <div className="card touch-base-card">
+            <div id='touch-base-card' className="card touch-base-card">
               <form autoComplete="false">
                 <input type="email" name="name" placeholder='Your email here...'/>
                 <textarea placeholder='Your message here...'></textarea>
