72a32dc2fc03e7f9243573f51c969c5832c32c3e
wilgru <william.gruszka@gmail.com>
2022-07-14 10:33:11 +1000

    added 2 new projects

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,6 +1,8 @@
 import project1image from './project_1.png';
 import project2image from './project_2.png';
 import project3image from './project_3.png';
+import project4image from './project_4.png';
+import project5image from './project_5.png';
 
 const projectData = [
     {
@@ -20,9 +22,23 @@ const projectData = [
     {
         title: "ProjectHub",
         image: project3image,
-        tags: ['Node.js', 'Express.js', 'Sequelize', 'Bootstrap'],
+        tags: ['Node.js', 'Express.js', 'Sequelize', 'Bootstrap', 'heroku'],
         githubLink: "https://github.com/wilgru/projecthub",
-        deployedLink: "projecthub-crm.herokuapp.com/"
+        deployedLink: "https://projecthub-crm.herokuapp.com/"
+    },
+    {
+        title: "Generic Tech Blog",
+        image: project4image,
+        tags: ['Node.js', 'Express.js', 'Sequelize', 'heroku'],
+        githubLink: "https://github.com/wilgru/USYDBC-WG-14-Tech-Blog",
+        deployedLink: "https://usydbc-14-tech-blog.herokuapp.com/"
+    },
+    {
+        title: "J.A.T.E - Just Another Text Editor",
+        image: project5image,
+        tags: ['Node.js', 'Express.js', 'Webpack', 'WorkBox'],
+        githubLink: "https://github.com/wilgru/USYDBC-WG-19-Text-Editor",
+        deployedLink: "https://usydbc-19-text-editor.herokuapp.com/"
     },
 ]
 
