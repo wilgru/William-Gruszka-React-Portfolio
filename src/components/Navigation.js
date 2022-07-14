e2f8e1552d4f47ab1e17f618763f3b532b49ba78
wilgru <william.gruszka@gmail.com>
2022-07-14 10:33:54 +1000

    added resume link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,10 @@
 const Navigation = () => {
     return (
         <nav className="App-nav">
-            <button className="plaque plaque-btn">Who Am I</button>
-            <button className="plaque plaque-btn">Work</button>
-            <button className="plaque plaque-btn">Touch Base</button>
+            <a className="plaque plaque-btn" href="#who-am-i-card">Who Am I</a>
+            <a className="plaque plaque-btn" href="#work-card">Work</a>
+            <a className="plaque plaque-btn" href="#touch-base-card">Touch Base</a>
+            <a className="plaque plaque-btn" href={process.env.PUBLIC_URL + '/William_Gruszka_Resume.pdf'} target="_blank">Resume</a>
         </nav>
     )
 };
