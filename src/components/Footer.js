37de5fe68c6353e9baa9b6f2d50761bf193a5b92
Your Name <you@example.com>
2022-09-07 13:28:54 +1000

    temporarily removing github and linkedin links from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <div id="App-footer">
-            <a className="plaque-btn" href='https://github.com/wilgru' target="_blank">GitHub</a>
-            <a className="plaque-btn" href='https://www.linkedin.com/in/william-gruszka-a03373227/' target="_blank">LinkedIn</a>
+            {/* <a className="plaque-btn" href='https://github.com/wilgru' target="_blank">GitHub</a>
+            <a className="plaque-btn" href='https://www.linkedin.com/in/william-gruszka-a03373227/' target="_blank">LinkedIn</a> */}
             <p>William Gruszka 2022</p>
         </div>
     )
