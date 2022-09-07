00355884480db809af4eb1da42bd01e3e3a88639
Your Name <you@example.com>
2022-09-07 13:34:50 +1000

    updated personal description

--- src/components/App.js
+++ src/components/App.js
@@ -65,7 +65,7 @@ function App() {
             </div>
             <div id='who-am-i-card' className="card card-bottom-buffer">
               <p>
-                Friendly and aspiring full-stack web developer motivated by a semantic and UX-conscious approach to web development and responsive web design, with interests in Progressive Web Apps and emerging technologies. Comes from a background in fast-paced optical retail, developing skills in communication, teamwork and attention to detail. Currently studying a certificate in web development through USYD’s Coding Bootcamp, developing skills with industry leading technologies like MERN Stack, PWA tooling including Webpack and Workbox, CSS libraries including Bootstrap, MVC and ORM/ODM.<br/>
+              Aspiring full-stack web developer motivated by a semantic and UX-conscious approach to web development and responsive web design, with an interest in Progressive Web Apps and learning emerging technologies. Comes from a background in fast-paced optical retail, developing skills in communication, teamwork and attention to detail. Completed a certificate in web development through USYD’s Coding Bootcamp, developing skills with industry leading technologies including MERN Stack, PWA tools like Webpack and Workbox, CSS libraries like Bootstrap and Ant Design, and paradigms like MVC and ORM/ODM.<br/>
                 <br/>
                 Looking for opportunities to utilise skills developed from prior experiences and current studies to build well designed and high quality web applications but also to leverage interests in problem solving, collaboration and innovation.
               </p>
